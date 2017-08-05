fbff1f27c9917cb5a03985884213bd40419445f0
nlathia <neal.lathia@cl.cam.ac.uk>
2017-08-05 15:23:30 +0100

    Update prepare for Darwin/Ubuntu

--- lesson1/prepare_data_for_validation.sh
+++ lesson1/prepare_data_for_validation.sh
@@ -1,15 +1,28 @@
 #!/usr/bin/env bash
 set -e
 
+unamestr=`uname -a`
+if [[ $unamestr == *"Ubuntu"* ]]; then
+  echo 'Using shuf on Ubuntu'
+  command="shuf"
+elif [[ $unamestr == *"Darwin"* ]]; then
+  echo 'Using gshuf on Darwin.'
+  brew update
+  brew install coreutils
+  command="gshuf"
+else
+  echo "Aborting: Installing shuf not implemented for: $unametr"
+  exit 1
+fi
+
 echo 'Split training data into train/validation...'
-# TODO gshuf is for Mac, won't work on AWS AMI (Ubuntu)
 
 source=data/train/cat
 target=data/valid/cat
 mkdir -p $target
-ls $source | gshuf -n 2000 | xargs -I '{}' mv "$source/{}" $target
+ls $source | $command -n 2000 | xargs -I '{}' mv "$source/{}" $target
 
 source=data/train/dog
 target=data/valid/dog
 mkdir -p $target
-ls $source | gshuf -n 2000 | xargs -I '{}' mv "$source/{}" $target
+ls $source | $command -n 2000 | xargs -I '{}' mv "$source/{}" $target
